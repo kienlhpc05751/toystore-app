12b66b2ffd11ec39cf146bb1121cf92c34a78096
kienlhpc05751 <139949358+kienlhpc05751@users.noreply.github.com>
2025-03-27 14:08:40 +0700

    Create StatisticsDAO.java

---
+++ src/com/toystore/dao/store/StatisticsDAO.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.toystore.dao.store;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.toystore.db.DatabaseConnection;
+
+public class StatisticsDAO {
+
+    private Connection conn;
+
+    public StatisticsDAO() {
+        try {
+            conn = DatabaseConnection.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Lấy tổng số user theo từng trạng thái
+    public int getUserCountByStatus(String status) {
+        String sql = "SELECT COUNT(*) FROM`order` WHERE status = ?";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, status);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    // Lấy tổng số lượng hàng trong kho
+    public int getTotalStock() {
+        String sql = "SELECT SUM(quantity) FROM product";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    // Lấy tổng doanh thu
+    public double getTotalRevenue() {
+        String sql = "SELECT SUM(TotalAmount) FROM `order`";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getDouble(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
